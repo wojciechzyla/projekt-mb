2b55c6d5a6dacfed32a93bb309aa796a32b7ebfa
wojciechzyla <62514065+wojciechzyla@users.noreply.github.com>
2023-05-24 20:36:47 +0200

    visualise clusters

--- clustering_api.py
+++ clustering_api.py
@@ -38,18 +38,15 @@ class ClusteringOCEL:
         event_ids = list(ocel.events["ocel:eid"])
         object_types = list(set(ocel.objects["ocel:type"]))
         object_type_to_id = {k: i for i, k in enumerate(object_types)}
-        activity_to_numeric = {k: i for i, k in enumerate(list(set(ocel.events["ocel:activity"])))}
         for eid in event_ids:
             eid_num = float(eid)
             if eid_num % 1000 == 0:
                 print(eid_num)
             counted = Counter(ocel.relations[ocel.relations["ocel:eid"] == eid]["ocel:type"])
-            event_vec = np.zeros(len(object_type_to_id) + 1)
+            event_vec = np.zeros(len(object_type_to_id))
             for k in counted.keys():
                 vec_id = object_type_to_id[k]
                 event_vec[vec_id] += counted[k]
-            activity = list(ocel.events[ocel.events["ocel:eid"] == eid]["ocel:activity"])[0]
-            event_vec[len(object_type_to_id)] = activity_to_numeric[activity]
             x.append(event_vec)
         return np.array(x)
 
